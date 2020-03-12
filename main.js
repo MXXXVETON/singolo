b296289b992e1c29e2bedc9262ac886bc5593a68
Arthur <kom9toshka@gmail.com>
2020-03-12 14:41:36 +0300

    -m

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-const MENU = document.getElementById('menu')
+const MENU = document.getElementById('menu');
+const MENUPORTFOLIO = document.getElementById('menu-portfolio');
+
 
 MENU.addEventListener('click', (event)=>{
-  MENU.querySelectorAll('li').forEach(el => el.classList.remove('active'))
+  MENU.querySelectorAll('li>a').forEach(el => el.classList.remove('active'))
 
   event.target.classList.add('active')
-})
+});
+
+MENUPORTFOLIO.addEventListener('click', (event)=>{
+  MENUPORTFOLIO.querySelectorAll('li>a').forEach(el => el.classList.remove('active'))
+
+  event.target.classList.add('active')
+});
+
